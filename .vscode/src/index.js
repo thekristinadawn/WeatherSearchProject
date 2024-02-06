99bc38633066b64f48921d06bc129460ac74e15e
thekristinadawn <thekristinadawn@gmail.com>
2024-02-06 09:56:43 -0600

    Added JS and moved files under VS Code

--- .vscode/src/index.js
+++ .vscode/src/index.js
@@ -2,7 +2,7 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
-  cityElement = searchInput.value;
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
