b2843a727f75cf1551a715dbe6c72d52d1c1939b
thekristinadawn <thekristinadawn@gmail.com>
2024-02-06 21:59:45 -0600

    Updated date into function

--- src/index.js
+++ src/index.js
@@ -6,27 +6,39 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-
   let date = new Date(response.data.time * 1000);
-
-  let dayDate = { weekday: "long" };
-  let formattedDate = date.toLocaleDateString("en-US", dayDate);
-
-  let time = { hour: "2-digit", minute: "2-digit", hour12: false };
-  let formattedTime = date.toLocaleTimeString("en-US", time);
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  dateElement.innerHTML = formattedDate;
-  timeElement.innerHTML = formattedTime;
+  timeElement.innerHTML = formatDate(date);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
   getForecast(response.data.city);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
